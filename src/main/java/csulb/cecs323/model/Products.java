771377ba824b98a3a9ea1fa1e665ce541e16ffec
royalty1free <54598612+royalty1free@users.noreply.github.com>
2021-10-19 13:59:42 -0700

    More Comment Updates

--- src/main/java/csulb/cecs323/model/Products.java
+++ src/main/java/csulb/cecs323/model/Products.java
@@ -74,56 +74,118 @@ public class Products {
         this.units_in_stock = units_in_stock;
     }
 
+    /**
+     * Default Constructor
+     */
     public Products() {}
 
     public String getUPC() {
         return UPC;
     }
 
+    /**
+     * A set method for UPC
+     *
+     * @param UPC
+     */
     public void setUPC(String UPC) {
         this.UPC = UPC;
     }
 
+    /**
+     * A get method for prod_name
+     *
+     * @return          current prod_name
+     */
     public String getProd_name() {
         return prod_name;
     }
 
+    /**
+     * A set method for prod_name
+     *
+     * @param prod_name
+     */
     public void setProd_name(String prod_name) {
         this.prod_name = prod_name;
     }
 
+    /**
+     * A get method for mfgr
+     *
+     * @return          current mfgr
+     */
     public String getMfgr() {
         return mfgr;
     }
 
+    /**
+     * A set method for mfgr
+     *
+     * @param mfgr
+     */
     public void setMfgr(String mfgr) {
         this.mfgr = mfgr;
     }
 
+    /**
+     * A get method for model
+     *
+     * @return          current model
+     */
     public String getModel() {
         return model;
     }
 
+    /**
+     * A set method for model
+     *
+     * @param model
+     */
     public void setModel(String model) {
         this.model = model;
     }
 
+    /**
+     * A get method unit_list_price
+     *
+     * @return          current unit_list_price
+     */
     public double getUnit_list_price() {
         return unit_list_price;
     }
 
+    /**
+     * A set method for unit_list_price
+     *
+     * @param unit_list_price
+     */
     public void setUnit_list_price(double unit_list_price) {
         this.unit_list_price = unit_list_price;
     }
 
+    /**
+     * A get method for units_in_stock
+     *
+     * @return          current units_in_stock
+     */
     public int getUnits_in_stock() {
         return units_in_stock;
     }
 
+    /**
+     * A set method for units_in_stock
+     *
+     * @param units_in_stock
+     */
     public void setUnits_in_stock(int units_in_stock) {
         this.units_in_stock = units_in_stock;
     }
 
+    /**
+     * A toString method for Products that displays the UPC, prod_name, unit_list_price, and units_in_stock
+     * return String            Data of product
+     */
     @Override
     public String toString () {
         return "Product- UPC: " + this.UPC + ", Name: " + this.prod_name + ", Price: " + this.unit_list_price
